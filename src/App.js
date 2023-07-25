ae8d567a4e54d650b7dc7c915c6d7cadc93d892d
MiaYinel <103024412+HAnh101@users.noreply.github.com>
2023-07-25 16:38:06 +0700

    props children

--- src/App.js
+++ src/App.js
@@ -1,99 +1,35 @@
 import React from 'react';
 import './App.css';
-import classNames from 'classnames';
 
-class TodoItem extends React.Component {
+class Accordion extends React.Component {
   render() {
-    const { item } = this.props
+    const { heading, children } = this.props
     
     return (
-      <div
-      onClick={() => this.props.onItemClick(item)}
-        className={classNames('todo-items', {
-          'done': item.isDone
-        })}
-        >
-        {item.title}
+      <div className='accordion'>
+        <h1>{heading}</h1>
+
+        <div className='content'>
+          {children}
+        </div>
       </div>
     )
   }
 }
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      'name': 'a',
-      count: 12,
-      todoItems: [
-        {
-          id: 1,
-          title: 'Cafe',
-          isDone: true,
-        },
-        {
-          id: 2,
-          title: 'Movie',
-          isDone: false,
-        },
-        {
-          id: 3,
-          title: '???',
-          isDone: false,
-        },
-      ]
-    }
-  }
-  
-  onItemClick(item) {
-    const _todoItemIndex = this.state.todoItems.findIndex(_item => _item.id === item.id)
-    const _todoItems = this.state.todoItems
-    _todoItems[_todoItemIndex].isDone = !_todoItems[_todoItemIndex].isDone
-
-    this.setState({
-      ...this.state,
-      todoItems: _todoItems
-    })
-  }
-
-  onInputChange(event) {
-    this.setState({ newTodoItem: event.target.value })
-  }
-
-  onAddNewTodoItem(event) {
-    if (event.keyCode === 13) {
-      this.setState({
-        todoItems: [
-          {
-            id: this.state.todoItems.length + 1,
-            title: this.state.newTodoItem,
-            isDone: false,
-          },
-          ...this.state.todoItems
-        ],
-        newTodoItem: ""
-      })
-      console.log(this.state.newTodoItem);
-    }
-  }
-
   render() {
     return (
-      <div className="App">
-        <input
-          onChange={(event) => this.onInputChange(event)}
-          value={this.state.newTodoItem} 
-          onKeyUp={(event) => this.onAddNewTodoItem(event)}
-        />
-        {
-          this.state.todoItems.map((_item, index) => {
-            return (
-              <TodoItem onItemClick={(item) => this.onItemClick(item)} item={_item} key={index} />
-            )
-          })
-        }
+      <div className='App'>
+        <Accordion 
+          heading="This is heading"
+        >
+          <div>
+            "This is content"
+          </div>
+        </Accordion>
       </div>
-    );
+    )
   }
 }
 
