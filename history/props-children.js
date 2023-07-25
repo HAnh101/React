ef6fe92996250f7738bfd611bcf07090dbb3f836
MiaYinel <103024412+HAnh101@users.noreply.github.com>
2023-07-25 16:45:01 +0700

    props children

---
+++ history/props-children.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+class Accordion extends React.Component {
+  render() {
+    const { heading, children } = this.props
+    
+    return (
+      <div className='accordion'>
+        <h1>{heading}</h1>
+
+        <div className='content'>
+          {children}
+        </div>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Accordion 
+          heading="This is heading"
+        >
+          <div>
+            "This is content"
+          </div>
+        </Accordion>
+      </div>
+    )
+  }
+}
+
+export default App;
