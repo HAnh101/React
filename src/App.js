c24d0e7e4822c7e98ea6f48eec8db22c45596842
MiaYinel <103024412+HAnh101@users.noreply.github.com>
2023-07-31 01:37:20 +0700

    Commit message

--- src/App.js
+++ src/App.js
@@ -1,97 +1,63 @@
 import React from 'react';
 import './App.css';
-import classNames from 'classnames';
 
-class TodoItem extends React.Component {
+const DEFAULT_VALUE = {
+  text: 'hello'
+}
+
+const CurrentContext = React.createContext(DEFAULT_VALUE)
+
+class App extends React.Component {
   render() {
-    const { item } = this.props
-    
     return (
-      <div
-      onClick={() => this.props.onItemClick(item)}
-        className={classNames('todo-items', {
-          'done': item.isDone
-        })}
-        >
-        {item.title}
+      <CurrentContext.Provider value={DEFAULT_VALUE}>
+        <_App />
+      </CurrentContext.Provider>
+    )
+  }
+}
+
+class _App extends React.Component {
+  static contextType = CurrentContext;
+
+  render() {
+    return (
+      <div className="App">
+        {this.context.text}
+        <Component1 />
+      </div>
+    );
+  }
+}
+
+class Component1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <Component2 />
       </div>
     )
   }
 }
 
-class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      'name': 'a',
-      count: 12,
-      todoItems: [
-        {
-          id: 1,
-          title: 'Cafe',
-          isDone: true,
-        },
-        {
-          id: 2,
-          title: 'Movie',
-          isDone: false,
-        },
-        {
-          id: 3,
-          title: '???',
-          isDone: false,
-        },
-      ]
-    }
-
-    this.inputRef = React.createRef()
-  }
-  
-  onItemClick(item) {
-    const _todoItemIndex = this.state.todoItems.findIndex(_item => _item.id === item.id)
-    const _todoItems = this.state.todoItems
-    _todoItems[_todoItemIndex].isDone = !_todoItems[_todoItemIndex].isDone
-
-    this.setState({
-      ...this.state,
-      todoItems: _todoItems
-    })
-  }
-
-  onAddNewTodoItem(event) {
-    if (event.keyCode === 13) {
-      this.setState({
-        todoItems: [
-          {
-            id: this.state.todoItems.length + 1,
-            title: this.inputRef.current.value,
-            isDone: false,
-          },
-          ...this.state.todoItems
-        ],
-      }, 
-      () => {
-        this.inputRef.current.value = ''
-      })
-    }
-  }
-
+class Component2 extends React.Component {
   render() {
     return (
-      <div className="App">
-        <input
-          ref={this.inputRef}
-          onKeyUp={(event) => this.onAddNewTodoItem(event)}
-        />
-        {
-          this.state.todoItems.map((_item, index) => {
-            return (
-              <TodoItem onItemClick={(item) => this.onItemClick(item)} item={_item} key={index} />
-            )
-          })
-        }
+      <div>
+        <Component3 />
       </div>
-    );
+    )
+  }
+}
+
+class Component3 extends React.Component {
+  static contextType = CurrentContext;
+  render() {
+    return (
+      <div>
+        {this.context.text}
+      </div>
+    )
   }
 }
 
